2f46d390cedc8b33e32357b740c2779ed0b35c48
Daniel STAN <daniel.stan@crans.org>
2013-06-14 23:42:38 +0200

    [dropbear/sysupgrade] save keys over flash (bis)

--- files/lib/upgrade/erase_all.sh
+++ files/lib/upgrade/erase_all.sh
@@ -1,9 +1,16 @@
+#!/bin/sh
+
+# Customisation Cr@ns : on ne garde que les clefs ssh lors d'un sysupgrade
+# le reste de la configuration est géré dans l'image ou via l'initscript
+# de first boot (hostname via dhcp par ex)
+
+# Surcharge de la fonction add_uci_conffiles de /sbin/sysupgrade
 add_uci_conffiles() {
         local file="$1"
-    	cat <<EOF 
+       	( cat <<EOF
 /etc/dropbear/dropbear_rsa_host_key
 /etc/dropbear/dropbear_dss_host_key
 EOF
-> "$file"
+) > $file
         return 0
 }
